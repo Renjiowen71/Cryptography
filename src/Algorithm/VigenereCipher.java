6299018d4c6dc3ae72dfb5d75ea5318a41adf02c
DylanBotha-Waikato <db132@students.waikato.ac.nz>
2023-10-18 13:00:06 +1300

    Added IC & letter frequnecy to Vigenere

--- src/Algorithm/VigenereCipher.java
+++ src/Algorithm/VigenereCipher.java
@@ -1,15 +1,25 @@
 package Algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import Algorithm.Interface.CryptAlg;
 
 public class VigenereCipher implements CryptAlg {
     private String key;
 
+    // Constructor that initializes the VigenereCipher with a given key
     public VigenereCipher(String key) {
         this.key = key.toUpperCase(); // Convert the key to uppercase for consistency
     }
 
+    // Utility method to remove spaces and convert to uppercase
+    public static String removeSpacesAndCapitals(String text) {
+        return text.replaceAll(" ", "").toUpperCase();
+    }
+
     @Override
+    // Encrypt a given message using the Vigenere cipher
     public String encrypt(String message) {
         StringBuilder ciphertext = new StringBuilder();
         int keyLength = key.length();
@@ -31,6 +41,7 @@ public class VigenereCipher implements CryptAlg {
     }
 
     @Override
+    // Decrypt a given cipher text using the Vigenere cipher
     public String decrypt(String cipher) {
         StringBuilder plaintext = new StringBuilder();
         int keyLength = key.length();
@@ -49,5 +60,48 @@ public class VigenereCipher implements CryptAlg {
         }
 
         return plaintext.toString();
+    }
+
+    // Calculate the Index of Coincidence (IC) for a given text
+    public double calculateIC(String text) {
+        int[] frequency = new int[26];
+        int total = 0;
+
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                frequency[c - 'A']++;
+                total++;
+            }
+        }
+
+        double ic = 0.0;
+        double totalDbl = (double) total;
+        
+        for (int f : frequency) {
+            ic += (f * (f - 1)) / (totalDbl * (totalDbl - 1));
+        }
+
+        return ic;
+    }
+
+
+    // Calculate the letter frequencies for a given text
+    public Map<Character, Double> calculateLetterFrequencies(String text) {
+        int[] frequency = new int[26];
+        int total = 0;
+
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                frequency[c - 'A']++;
+                total++;
+            }
+        }
+
+        Map<Character, Double> frequencies = new HashMap<>();
+        for (char c = 'A'; c <= 'Z'; c++) {
+            frequencies.put(c, frequency[c - 'A'] / (double) total);
+        }
+
+        return frequencies;
     }
 }
